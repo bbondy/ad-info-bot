f42ba580f3a99cb936c5ae66db21fccd484a8069
Brian R. Bondy <netzen@gmail.com>
2015-10-01 15:47:48 -0400

    Hookup ad-info for amqp

--- src/processSites.js
+++ src/processSites.js
@@ -1,7 +1,15 @@
 var amqp = require('amqplib/callback_api');
+let fs = require('fs');
+import {init, getAdInfo, exit} from 'ad-info';
+
 const queueName = 'ad-location-queue';
 
 var amqp = require('amqplib/callback_api');
+
+let perSiteInfo = {};
+if (fs.existsSync('./perSiteInfo.json')) {
+  perSiteInfo = JSON.parse(fs.readFileSync('./perSiteInfo.json', 'utf-8'));
+}
 
 console.log('[*] Waiting for messages. To exit press CTRL+C');
 
@@ -12,24 +20,34 @@ function handleError(err) {
   }
 }
 
-amqp.connect('amqp://localhost', (err, conn) => {
-  handleError(err);
-  conn.createChannel((connErr, ch) => {
-    handleError(connErr);
-    var q = 'ad-info-queue';
-    ch.assertQueue(queueName, {durable: true});
+let sequence = init('./node_modules/ad-info/data/easylist.txt').then(() => {
+  amqp.connect('amqp://localhost', (err, conn) => {
+    handleError(err);
+    conn.createChannel((connErr, ch) => {
+      handleError(connErr);
+      var q = 'ad-info-queue';
+      ch.assertQueue(queueName, {durable: true});
 
-    // Only send 1 message until previous one is ack'ed
-    ch.prefetch(1);
+      // Only send 1 message until previous one is ack'ed
+      ch.prefetch(1);
 
-    ch.consume(queueName, (msg) => {
-      setTimeout(() => {
-        console.log(" [x] Processed %s", msg.content.toString());
-        // Tell the queue we processed the site
-        ch.ack(msg);
-      }, 1000);
-    }, {
-      noAck: false
+      ch.consume(queueName, (msg) => {
+        let siteHost = msg.content.toString();
+        console.log(`http://${siteHost}`);
+
+        getAdInfo(`http://${siteHost}`).then((info) => {
+          perSiteInfo[siteHost] = info;
+          fs.writeFileSync('./perSiteInfo.json', JSON.stringify(perSiteInfo));
+          // Tell the queue we processed the site
+          ch.ack(msg);
+        }).catch((err) => {
+          console.warn('Error obtaining info for site host:', siteHost, '. Error:', err);
+          // Tell the queue we processed the site. This queue will be refilled periodically anyway.
+          ch.ack(msg);
+        });
+      }, {
+        noAck: false
+      });
     });
   });
 });
