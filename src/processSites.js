d893cbeb994752fa01490c9c5431e0348e0147df
Brian R. Bondy <netzen@gmail.com>
2015-10-01 16:11:01 -0400

    Add verbose option mode

--- src/processSites.js
+++ src/processSites.js
@@ -1,14 +1,25 @@
 var amqp = require('amqplib/callback_api');
 let fs = require('fs');
 import {init, getAdInfo, exit} from 'ad-info';
-
-const queueName = 'ad-location-queue';
-
 var amqp = require('amqplib/callback_api');
 
+// If true, output file includes:
+// - numResourcesRequested (Number)
+// - numResourcesBlocked (Number)
+// - resourcesBlocked (Array)
+// - iframesData (Array)
+// - pageLoadTime (number of milliseconds it took to load the page)
+// - abpTime (number of milliseconds spent using ad block plus filter lookups on each URL)
+// If false, only includes an Array of the iframe data.
+// Queues and output filenames will differ too.
+let verbose = false;
+const verboseSuffix = verbose ? '-verbose' : '';
+const queueName = 'ad-location-queue' + verboseSuffix;
+const jsonFilename = './perSiteInfo' + verboseSuffix + '.json'
+
 let perSiteInfo = {};
-if (fs.existsSync('./perSiteInfo.json')) {
-  perSiteInfo = JSON.parse(fs.readFileSync('./perSiteInfo.json', 'utf-8'));
+if (fs.existsSync(jsonFilename)) {
+  perSiteInfo = JSON.parse(fs.readFileSync(jsonFilename, 'utf-8'));
 }
 
 console.log('[*] Waiting for messages. To exit press CTRL+C');
@@ -37,9 +48,14 @@ let sequence = init('./node_modules/ad-info/data/easylist.txt').then(() => {
 
         getAdInfo(`http://${siteHost}`).then((info) => {
           // To save space, only add information if there are ads on the page
-          if (info.length > 0) {
+          if (verbose) {
             perSiteInfo[siteHost] = info;
-            fs.writeFileSync('./perSiteInfo.json', JSON.stringify(perSiteInfo));
+            fs.writeFileSync(jsonFilename, JSON.stringify(perSiteInfo));
+          // In non verbose mode there's no entropy for storing empty arrays per site,
+          // it's better to save the space.
+          } else if (info.iframesData.length > 0) {
+            perSiteInfo[siteHost] = info.iframesData;
+            fs.writeFileSync(jsonFilename, JSON.stringify(perSiteInfo));
           }
           // Tell the queue we processed the site
           ch.ack(msg);
